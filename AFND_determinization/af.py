932cdd97f03c4c44b1da881288fb41402266278a
Eduardo Perottoni <edu.perottoni@gmail.com>
2023-09-20 23:00:10 -0300

    Determinization passed :)

--- AFND_determinization/af.py
+++ AFND_determinization/af.py
@@ -1,4 +1,5 @@
 from .transition import Transition
+import logging
 
 class AF:
     """
@@ -13,23 +14,34 @@ class AF:
         self.__load_from_input(input)
         self.__states = self.__order(self.__states)
 
-        self.__compute_e_closures()
-        self.__rebuild_initial_state()
-        self.__build_transitions_by_source()
-        self.determinize()
 
     def __order(self, collection: set[str] | list[str]) -> list[str]:
         return sorted(collection)
+    
+
+    def __filter_transitions(self, symbol: str) -> set[Transition]:
+        """
+        Seach for the transitions that have this specific symbol
+        """
+        filtered = set()
+        for t in self.__transitions:
+            if t.symbol == symbol:
+                filtered.add(t)
+        return filtered
 
 
     def __build_transitions_by_source(self) -> None:
+        """
+        Build a dict of transitions with source state as dict key
+        """
         for transition in self.__transitions:
             if transition.source in self.__transitions_by_source.keys():
                 self.__transitions_by_source[transition.source].add(transition)
             else:
                 self.__transitions_by_source[transition.source] = {transition, }
 
-        # print(f'Transições por source: {self.__transitions_by_source}')
+        # logging.debug(f'Transições por source: {self.__transitions_by_source}')
+
 
     def __compute_e_closures(self):
         if self.__has_e_transitions:
@@ -37,7 +49,27 @@ class AF:
         else:
             self.__generate_simple_e_closures()
         
-        # print(f'&-fechos: {self.__e_closure}')
+        # logging.debug(f'&-fechos: {self.__e_closure}')
+
+
+    def __generate_simple_e_closures(self):
+        """
+        &-closure is the state itself
+        """
+        for state in self.__states:
+            self.__e_closure[state] = {state}
+
+    
+    def __generate_deep_e_closures(self):
+        """
+        Generates &-closure of the states based on &-transitions
+        """
+        for state in self.__states:
+            # logging.debug(f'BUSCANDO FECHO DO ESTADO {state}')
+            e_closure: set[str] = set(state)
+            self.__e_closure[state] = self.__deep_search_e_closure(e_closure, 
+                                                                   state, 
+                                                                   self.__filter_transitions('&'))
 
 
     def __deep_search_e_closure(self, e_closure: set[str], state: str, transitions: set[Transition]):
@@ -50,31 +82,12 @@ class AF:
                 for target in transition.target:
                     return self.__deep_search_e_closure(e_closure, target, transitions)
         return e_closure
-    
-
-    def __generate_deep_e_closures(self):
-        for state in self.__states:
-            # print(f'BUSCANDO FECHO DO ESTADO {state}')
-            e_closure: set[str] = set(state)
-            self.__e_closure[state] = self.__deep_search_e_closure(e_closure, 
-                                                                   state, 
-                                                                   self.__filter_transitions('&'))
-
-
-    def __generate_simple_e_closures(self):
-        for state in self.__states:
-            self.__e_closure[state] = {state}
-
-
-    def __filter_transitions(self, symbol: str) -> set[Transition]:
-        filtered = set()
-        for t in self.__transitions:
-            if t.symbol == symbol:
-                filtered.add(t)
-        return filtered
 
 
     def __load_from_input(self, input: str) -> None:
+        """
+        Loads the Automata from a string
+        """
         splited = input.split(';')
 
         states_qtt = int(splited[0])
@@ -110,13 +123,12 @@ class AF:
         
         assert states_qtt == len(self.__states)
 
-
-        # print(f'Estado inicial: {self.__initial_state}')
-        # print(f'Estados finais: {self.__final_states}')
-        # print(f'Estados: {self.__states}')
-        # print(f'Alfabeto: {self.__alphabet}')
-        # print(f'Transitions: {self.__transitions}')
-        # print(f'Has &-transitions: {self.__has_e_transitions}')
+        logging.debug(f'Estado inicial: {self.__initial_state}')
+        logging.debug(f'Estados finais: {self.__final_states}')
+        logging.debug(f'Estados: {self.__states}')
+        logging.debug(f'Alfabeto: {self.__alphabet}')
+        logging.debug(f'Transitions: {self.__transitions}')
+        logging.debug(f'Has &-transitions: {self.__has_e_transitions}')
 
 
     def __rebuild_initial_state(self) -> None:
@@ -125,7 +137,7 @@ class AF:
         """
         self.__initial_state = self.__e_closure[self.__initial_state]
 
-        # print(f'STATES REBUILDED: {self.__states}')
+        # logging.debug(f'STATES REBUILDED: {self.__states}')
 
 
     def __get_transition_by_source_and_symbol(self, source: str, symbol: str) -> Transition | None:
@@ -136,13 +148,27 @@ class AF:
         return None
 
 
-    def determinize(self):
+    def determinize(self) -> None:
+        """
+        Macro algorithm to determinization
+        """
+        self.__compute_e_closures()
+        self.__rebuild_initial_state()
+        self.__build_transitions_by_source()
+        self.__execute_determinization()
+
+
+    def __execute_determinization(self):
+        """
+        Execute main algorith of determinization
+        """
         to_visit_list = [self.__initial_state]
         visited_list = []
         print_information = {
             'transitions': [],
             'states': []
         }
+        self.__determinized_transitions = []
         while len(to_visit_list) > 0:
 
             state = to_visit_list.pop(0)
@@ -161,12 +187,8 @@ class AF:
                                     tmp_result |= self.__e_closure[target]
 
                     if tmp_result:
-                        print_information['transitions'].append({
-                            'source': state,
-                            'symbol': symbol,
-                            'target': tmp_result
-                        })
-                        print(f'TRANSIÇÃO: {state} + {symbol} -> {tmp_result}')
+                        self.__determinized_transitions.append(Transition(state, symbol, tmp_result))
+                        # print(f'TRANSIÇÃO: {state} + {symbol} -> {tmp_result}')
                     
                     insert = bool(tmp_result)
                     for item in to_visit_list + visited_list:
@@ -176,24 +198,28 @@ class AF:
                     if insert: 
                         to_visit_list.append(tmp_result)
 
-        print_information['states'] = visited_list
+        self.__determinized_states = visited_list
 
-        self.__print_determinized_af(print_information) 
+        self.__format_and_print_af(self.__determinized_states, self.__determinized_transitions) 
 
-    def __print_determinized_af(self, information: dict) -> None:
+
+    def __format_and_print_af(self, states_list: list[set[str]], transitions_list: list[Transition]) -> None:
+        """
+        Formats the output string and print the result
+        """
+        
         ## Número de estados do autômao determinizado
-        number_of_states = len(information['states'])
+        number_of_states = len(states_list)
 
         ## Novos estados de aceitação
         final_states = []
-        for state in information['states']:
+        for state in states_list:
             # Verifica quais dos estados do novo autômato são de aceitação
             if state & self.__final_states and state not in final_states:
                 final_states.append(state)
 
         ordered_final_states = []
         for state in final_states:
-            ordered_state  = self.__order(state)
             ordered_final_states.append(
                 f'{{{"".join(self.__order(state))}}}'
             )
@@ -205,15 +231,24 @@ class AF:
         alphabet_str = f'{{{",".join(alphabet)}}}'
 
         ## Estado inicial
-        initial_state_str = f'{{{"".join(self.__order(information["states"][0]))}}}'
+        initial_state_str = f'{{{"".join(self.__order(states_list[0]))}}}'
 
         ## Transições
-        ordered_transitions = []
-        for t in information['transitions']:
-            ordered_transitions.append(
+        transitions = [{'source': t.source, 'symbol': t.symbol, 'target': t.target} for t in transitions_list]
+
+        for t in transitions:
+            t['source'] = "".join(self.__order(t['source']))
+
+        ordered_transitions = sorted(transitions, key=lambda x: (x['source'], x['source']))
+
+        ordered_transitions_str = []
+        for t in ordered_transitions:
+            # Ordena as transições pelo source da transição
+            ordered_transitions_str.append(
                 f'{{{"".join(self.__order(t["source"]))}}},{t["symbol"]},{{{"".join(self.__order(t["target"]))}}}'
             )
-        ordered_transitions_str = ';'.join(ordered_transitions)
+            
+        ordered_transitions_str = ';'.join(ordered_transitions_str)
         
         final_str = ';'.join([str(number_of_states), initial_state_str, ordered_final_states_str, alphabet_str, ordered_transitions_str])
 
