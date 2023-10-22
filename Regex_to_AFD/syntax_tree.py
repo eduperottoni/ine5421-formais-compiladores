730f5f27415dd6024e0ba40d453b7afd4d060521
Eduardo Perottoni <edu.perottoni@gmail.com>
2023-10-22 02:54:52 -0300

    Syntax Tree building well for all test cases

---
+++ Regex_to_AFD/syntax_tree.py
@@ -0,0 +1,186 @@
+from collections import deque
+# from typing import Self
+from PrettyPrint import PrettyPrintTree
+
+class SyntaxTreeNode:
+    """
+    This class is a node of the Syntax Tree
+    """
+    def __init__(self, symbol: str):
+        # Symbol: a operator or an item of the alphabet
+        self.symbol = symbol
+        # Information needed to AFD tranmformation
+        self.nullable = symbol == '*'
+        self.first_pos = None
+        self.last_pos = None
+        # Child nodes
+        self.__children = []
+
+    @property
+    def children(self) -> 'list[Self]':
+        return self.__children
+    
+    @property
+    def first_child(self) -> 'Self | None':
+        return self.__get_child(0)
+
+    @property
+    def last_child(self) -> 'Self | None':
+        return self.__get_child(1)
+
+    def __get_child(self, index: int) -> 'Self | None':
+        """
+        Reusable function to get specified child from list
+        """
+        try:
+            return self.__children[index]
+        except:
+            return None
+
+    def append_child(self, child: 'Self') -> None:
+        self.__children.append(child)
+
+
+class SyntaxTree:
+    """
+    Class to represent the Syntax Tree
+    """
+    def __init__(self, regex: str, operators: tuple[str]):
+        self.__define_alphabet(regex, operators)
+        self.__complete_regex(regex)
+        self.root = SyntaxTreeNode(None)
+        self.aux_stack = deque()
+        self.__create_tree(self.completed_regex, self.root)
+
+    
+    def __define_alphabet(self, regex: str, operators: dict[str | str]) -> None:
+        """
+        We define here wich are the symbols of the Regex
+        """
+        alphabet = {i for i in f'({regex})#' if i not in operators.values()}
+        self.alphabet = alphabet
+        print(f'Alphabet defined: {self.alphabet}')
+
+    
+    def __complete_regex(self, regex: str) -> None:
+        """
+        Adds the final symmbol ('#') and the concats ('.') in the regex 
+        """
+        masked_regex = f'({regex})#'
+        new_regex = ''
+        
+        for index, item in enumerate(masked_regex[:-1]):
+            next_item = masked_regex[index + 1]
+            if item in self.alphabet:
+                new_regex += item if next_item in [')', '*', '|'] else f'{item}.'
+            elif item == ')':
+                new_regex += item if next_item in ['*', ')'] else f'{item}.'
+            elif item == '*':
+                new_regex += item if next_item in ['.', '|', ')'] else f'{item}.'
+            else:
+                new_regex += item
+
+        self.completed_regex = f'{new_regex}#'
+        print(f"Regex's completeness result: {self.completed_regex}")
+
+
+    def __create_tree(self, regex: str, root: SyntaxTreeNode) -> None:
+        """
+        Recursive function that creates nodes from a root one
+        """
+        # Base case (we have just one symbol)
+        if len(regex) == 1:
+            root.symbol = regex
+        # If we have operations -> we call this function recursvely
+        else:
+            # 1 - We find out the position of the operator which can split the regex
+            operator, position = self.__search_operator_position(regex)
+            # 2 - This operator will be the root of the new subtree
+            root.symbol = operator
+            # 3 - We get the splitted parts 
+            left, right = self.__divide_regex(regex, position)
+            print(f'Calling recursively: {left}')
+            print(f'Calling recursively: {right}')
+            # 4 - We create new nodes and call recursively to each one
+            root.append_child(SyntaxTreeNode(None))
+            self.__create_tree(left, root.first_child)
+            if right:
+                root.append_child(SyntaxTreeNode(None))
+                self.__create_tree(right, root.last_child)
+
+
+    def __search_operator_position(self, regex):
+        """
+        Considering the regex, we found out the position of the last
+        occurrence of one of the operators, in inverse order of precedence
+        """
+        print(f'Searching operator {regex}')
+        # We search in the inverse order of natural precedence
+        for oper in ['|', '.', '*']:
+            self.aux_stack.clear()
+            for index, item in enumerate(regex[::-1]):
+                # We are passign through a new level of depth
+                if item == ')': self.aux_stack.append(item)
+                # We are leaving a deeper level
+                elif item == '(': self.aux_stack.pop()
+                # We could only consider current level of depth
+                elif not bool(self.aux_stack):
+                    if item == oper:
+                        return oper, len(regex) - index - 1
+
+
+    def __divide_regex(self, regex: str, position: int) -> tuple[str, str]:
+        """
+        We split the regex in two parts based on the position of the
+        operator with less precedence
+        """
+        # If position is zero, we are in case of a * operation
+        left = regex[:position]
+        right = regex[position + 1 :] if position > 0 else None
+
+        new_list = []
+        # 1 - We discard unecessary parenthesis
+        for i in [left, right]:
+            if i:
+                has_extra, carac = self.__has_extra_parenthesis(i)
+                if not has_extra:
+                    if i[0] == '(' and i[-1] != '*': i = i[1:]
+                    if i[-1] == ')': i = i[:-1]
+                else:
+                    if carac == '(':
+                        i.replace(carac, '', 1)
+                    elif carac == ')':
+                        rev_s = i[::-1]
+                        i = len(i) - rev_s.index(carac) - 1
+                        i = i[:i] + i[i+1:]
+            new_list.append(i)
+
+        return new_list
+
+
+    def __has_extra_parenthesis(self, regex: str) -> (bool, str):
+        """
+        Analyses whether the regex contains extra parenthesis
+        """        
+        stack = deque()
+        for item in regex:
+            if item == '(': stack.append(item)
+            elif item == ')':
+                # If empty while reading ')'
+                if not bool(stack):
+                    return True, ')'
+                else:
+                    stack.pop()
+
+        # To be validated, stack must be empty
+        is_empty = not bool(stack)
+
+        return not is_empty, '(' if not is_empty else None
+
+
+    def pretty_print(self):
+        """
+        Using PrettyPrintTree lib to print our Syntax Tree
+        """
+        pt = PrettyPrintTree(lambda x: x.children, lambda x: x.symbol)
+        pt(self.root)
--- Regex_to_AFD/syntax_tree_builder.py
+++ Regex_to_AFD/syntax_tree_builder.py
@@ -1,61 +1,17 @@
-class SyntaxTree():
-    def __init__(self, regex: str, operators: tuple[str]):
-        self.__define_alphabet(regex, operators)
-        self.__mask_regex(regex, operators)
-        # self.__parse_regex(regex)
-
-
-    def __mask_regex(self, regex: str, operators: dict) -> None:
-        """
-        Adiciona # ao final da palavra
-        Adiciona . nos lugares que estão faltando
-        """
-        masked_regex = f'({regex})#'
-        # print(f'SELF_MASKED_REGEX: {self.masked_regex}')
-        new_regex = ''
-        
-        for index, item in enumerate(masked_regex[:-1]):
-            # print(f'CURR_ELEMENT: {item}')
-            next_item = masked_regex[index + 1]
-            # print(f'NEXT_ELEMENT: {next_item}')
-            if item in self.alphabet:
-                # Case 'ab', 'a('
-                new_regex += item if next_item in operators.values() and next_item != operators['par_initial'] else f'{item}.'
-            elif item == operators['par_final']:
-                #Case ')*', '))'
-                new_regex += item if next_item in [operators['star'], operators['par_final']] else f'{item}.'
-            elif item == operators['star']:
-                new_regex += item if next_item in operators.values() and next_item != operators['par_initial'] else f'{item}.'
-            else:
-                new_regex += item
-            # print(f'=>{new_regex}')
-
-        self.completed_regex = f'{new_regex}#'
-        print(self.completed_regex)
-
-
-    def __define_alphabet(self, regex: str, operators: dict[str | str]) -> None:
-        """
-        We define here wich are the symbols of the Regex
-        """
-        alphabet = {i for i in f'({regex})#' if i not in operators.values()}
-        self.alphabet = alphabet
-
-
-    # def __parse_regex(self, regex):
-    #     self.__alphabet = 
-
+from syntax_tree import SyntaxTree
 
 class SyntaxTreeBuilder():
-    
+    """
+    Syntax Tree builder
+    """
+
     operators = {
         'concat': '.',
         'or': '|',
         'star': '*',
-        'par_initial': '(',
-        'par_final': ')'
+        'open_par': '(',
+        'close_par': ')'
     }
-
 
     @classmethod
     def build_tree(cls, regex: str) -> SyntaxTree:
